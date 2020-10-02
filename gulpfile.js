2f9c02bdf880dc2b7c6d35659e4492da6f7928f7
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2020-10-02 23:32:46 +0300

    start settings fonts

--- gulpfile.js
+++ gulpfile.js
@@ -172,12 +172,27 @@ gulp.task('svgSprite', function () {
 })
 
 function fontsStyle() {
-  // body
+  const fs = require('fs');
+  let file_content = fs.readFileSync(source_folder + '/scss/fonts.scss');
+  if (file_content == '') {
+    fs.writeFile(source_folder + '/scss/fonts.scss', '', cb);
+    return fs.readdir(path.build.fonts, function (err, items) {
+      if (items) {
+        let c_fontname;
+        for (var i = 0; i < items.length; i++) {
+          let fontname = items[i].split('.');
+          fontname = fontname[0];
+          if (c_fontname != fontname) {
+            fs.appendFile(source_folder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
+          }
+          c_fontname = fontname;
+        }
+      }
+    })
+  }
 }
 
-function cb() {
-  // body
-}
+function cb() {}
 
 function watchFiles(arguments) { // Функция слежки за файлами
   gulp.watch([path.watch.html], html); // Слежка за HTML файлами
@@ -193,6 +208,7 @@ function clean() {
 let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts), fontsStyle);
 let watch = gulp.parallel(build, watchFiles, browserSync); // Сценарий выполнения функций
 
+exports.fontsStyle = fontsStyle;
 exports.fonts = fonts;
 exports.images = images;
 exports.js = js;
